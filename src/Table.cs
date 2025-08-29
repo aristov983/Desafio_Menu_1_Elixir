4585b823d92e7c1e1efd730bdf6e0480472b53c8
aristov983 <earis4499@gmail.com>
2025-08-29 20:39:38 -0300

    Clase Table

--- src/Table.cs
+++ src/Table.cs
@@ -8,8 +8,45 @@ namespace Ucu.Poo.Restaurant
     public class Table
     {
         private List<Dish> order = new List<Dish>();
+        private int number;
+        private bool isoccupied = false;
 
-        public bool HasOrders()
+        public int Number
+        {
+            get
+            {
+                return number;
+            }
+            set
+            {
+                number = value;
+            }
+        }
+
+        public List<Dish> Order
+        {
+            get
+            {
+                return order;
+            }
+        }
+
+        public void Occupy() //Marca la mesa como ocupada
+        {
+            isoccupied = true;
+        }
+
+        public void Free() //Marca la mesa como libre
+        {
+            isoccupied = false;
+        }
+
+        public void AddToOrder(Dish plate) //agrega el plato plate a la lista order
+        {
+            order.Add(plate);
+        }
+        
+        public bool HasOrders() //Devuelve true si hay alguna orden en la mesa
         {
             return this.order.Count > 0;
         }
