325335fcbe99e6b314c9fd775df88f0397c97912
Lucas Chiappinigit <chiappinilucas@gmail.com>
2025-08-29 20:30:08 -0300

    Se creo la clase Dish, esta clase tiene sus atributos y su constructor

--- src/Dish.cs
+++ src/Dish.cs
@@ -4,7 +4,23 @@ namespace Ucu.Poo.Restaurant
     /// <summary>
     /// Representa un platillo individual en el menú del restaurante.
     /// </summary>
-    public class Dish
+    public class Dish 
     {
+        public string Name {get; set;} // Es otra forma de poner los get y set. La funcion es la misma.
+        
+        public float Price { get; set; }
+        
+        public bool IsVegetarian { get; set; } //Si devuelve true es vegano. Sino no es vegano.
+
+        public Dish(string name, float price, bool isVegetarian) // Constructor
+        {
+            Name = name; // Le asigno al atributo el valor que me pasan. Aca a la propiedad con mayuscula Name(de la clase), se le asigna el nombre que se le ponen al crear la instancia, ejemplo: "Milanesa Napolitana"
+            Price = price;
+            IsVegetarian = isVegetarian;
+        }
+        
+        
+      
+       
     }
 }
