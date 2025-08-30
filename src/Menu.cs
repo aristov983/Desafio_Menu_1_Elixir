9caf36ad4ccbf63b710756495acbc66889934900
NicolasDiazAg <NICOLAS.DIAZS@correo.ucu.edu.uy>
2025-08-29 22:49:12 -0300

    Clase menu: Menu.cs

--- src/Menu.cs
+++ src/Menu.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Collections.Generic;
+using System.Xml;
 
 namespace Ucu.Poo.Restaurant
 {
@@ -8,6 +10,45 @@ namespace Ucu.Poo.Restaurant
     /// </summary>
     public class Menu
     {
-        private List<Dish> dishes = new List<Dish>();
+        public List<Dish> dishes = new List<Dish>();
+
+        public class Dish
+        {
+            public string Name { get; set; }
+            public decimal Price { get; set; }
+
+            public Dish(string name, decimal price)
+            {
+                Name = name;
+                Price = price;
+            }
+
+            public override string ToString()
+            {
+                return $"{Name} - ${Price}";
+            }
+        }
+
+        public void AddDish(Dish dish)
+        {
+            dishes.Add(dish);
+        }
+
+        public void DellDish(Dish dish)
+        {
+            dishes.Remove(dish);
+        }
+
+        public Dish GetDishByName(string name)
+        {
+            foreach (Dish d in dishes)
+            {
+                if (d.Name == name)
+                {
+                    return d;
+                }
+            }
+            return null;
+        }
     }
 }
